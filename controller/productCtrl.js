d95d587bb15c7fc64dfe1740752b79f17556d9fb
Dash10107 <dakshcjain@gmail.com>
2024-08-08 14:35:45 +0530

    slightly populated user in ratings in product

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -52,7 +52,7 @@ const createProduct = asyncHandler(async (req, res) => {
     const { id } = req.params;
     validateMongoDbId(id);
     try {
-      const findProduct = await Product.findById(id);
+      const findProduct = await Product.findById(id).populate("ratings.postedby");
       res.json(findProduct);
     } catch (error) {
       throw new Error(error);
